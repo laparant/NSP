798c81cdc20067b496631c318a6a4007c941922f
laparant <laurie-anne.parant@uqac.ca>
2018-04-11 21:54:35 -0400

    Modif de swap

--- findNeighbor.m
+++ findNeighbor.m
@@ -2,17 +2,19 @@ function [bestS] = findNeighbor(P,S)
 bestS = S;
 bestOV = ObjectiveValue(P,S);
 
-% Ligne fixée pour le swap
-i_fixed = randi(size(S,1));
+% Pour chaque ligne, faire tous les swaps possibles
 for i=1:1:size(S,1)
-% Effectuer le swap
-A = S;
-A(i,:) = S(i_fixed,:);
-A(i_fixed,:) = S(i,:);
-% Calculer le coût et enregistrer la solution si meilleure
-if IsLegal(P,A) && ObjectiveValue(P,A) < bestOV
-    bestOV = ObjectiveValue(P,A);
-    bestS = A;
-end
+    i_fixed = i;
+    for j=1:1:size(S,1)
+        % Effectuer le swap
+        A = S;
+        A(j,:) = S(i_fixed,:);
+        A(i_fixed,:) = S(j,:);
+        % Calculer le coût et enregistrer la solution si meilleure
+        if IsLegal(P,A) && ObjectiveValue(P,A) < bestOV
+            bestOV = ObjectiveValue(P,A);
+            bestS = A;
+        end
+    end
 end
 end
