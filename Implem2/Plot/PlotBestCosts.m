9ae734d64c4b626d5435c29785d4c7493a1424d3
laparant <laurie-anne.parant@uqac.ca>
2018-04-17 12:40:50 -0400

    Title added to plot

--- Implem2/Plot/PlotBestCosts.m
+++ Implem2/Plot/PlotBestCosts.m
@@ -1,6 +1,7 @@
 function PlotBestCosts(BestCost)
 plot(0:size(BestCost,2)-1,BestCost,'r-x');
 xlabel('Iteration');
-ylabel('Best Cost');
+ylabel('Best fitness value');
+title('Best fitness value over iteration')
 grid on;
 end
